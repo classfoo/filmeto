8e6bc7fae8d7c9577a9336e8a9bb91e42d688240
classfoo <fuzhiqin@sina.com>
2025-10-18 10:19:46 +0800

    code

--- app/ui/export_video/__init__.py
+++ app/ui/export_video/__init__.py
@@ -1,7 +1,6 @@
 """
 Export video UI module initialization
 """
-from .export_dialog import ExportDialog
 from .export_video_widget import ExportVideoWidget
 
-__all__ = ['ExportDialog', 'ExportVideoWidget']
+__all__ = ['ExportVideoWidget']
